d13889f1e64dc96f9ba1a97398f4c32c70fd1951
Tatiana Gazaryan <65111218+tgazaryan@users.noreply.github.com>
2020-09-09 12:15:50 +0300

    Update script__form.js

--- source/js/script__form.js
+++ source/js/script__form.js
@@ -1,8 +1,8 @@
 var reviewForm = document.querySelector(".review-form");
-var sentPopup = document.querySelector(".page-modal__sent");
-var errorPopup = document.querySelector(".page-modal__error");
-var closeSent = document.querySelector(".button__sent");
-var closeError = document.querySelector(".button__error");
+var sentPopup = document.querySelector(".page-modal--sent");
+var errorPopup = document.querySelector(".page-modal--error");
+var closeSent = document.querySelector(".button--sent");
+var closeError = document.querySelector(".button--error");
 var formName = document.querySelector("#name");
 var formSurname = document.querySelector("#surname");
 var formPhone = document.querySelector("#phone");
@@ -11,33 +11,33 @@ var formMail = document.querySelector("#mail");
 reviewForm.addEventListener("submit", function(evt) {
   if (!formName.value||!formSurname.value||!formPhone.value||!formMail.value) {
     evt.preventDefault ();
-    errorPopup.classList.add("page-modal__error--show");
+    errorPopup.classList.add("page-modal--show");
   } else {
     evt.preventDefault();
-    sentPopup.classList.add("page-modal__sent--show");
+    sentPopup.classList.add("page-modal--show");
   }
 });
 
 closeSent.addEventListener ("click", function(evt) {
     evt.preventDefault();
-    sentPopup.classList.remove("page-modal__sent--show");
+    sentPopup.classList.remove("page-modal--show");
 });
 
 closeError.addEventListener ("click", function(evt) {
     evt.preventDefault();
-    errorPopup.classList.remove("page-modal__error--show");
+    errorPopup.classList.remove("page-modal--show");
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    if (sentPopup.classList.contains("page-modal__sent--show")) {
+    if (sentPopup.classList.contains("page-modal--show")) {
       evt.preventDefault();
-      sentPopup.classList.remove("page-modal__sent--show");
+      sentPopup.classList.remove("page-modal--show");
     }
 
-    if (errorPopup.classList.contains("page-modal__error--show")) {
+    if (errorPopup.classList.contains("page-modal--show")) {
       evt.preventDefault();
-      errorPopup.classList.remove("page-modal__error--show");
+      errorPopup.classList.remove("page-modal--show");
     }
   }
 });
